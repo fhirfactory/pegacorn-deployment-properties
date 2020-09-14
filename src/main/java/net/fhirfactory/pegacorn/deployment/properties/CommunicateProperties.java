d31d7655c38a73bd743df05e9b84eebd0a266061
mark-hunter <49222848+mark-hunter@users.noreply.github.com>
2020-09-14 15:16:03 +1000

    Updated for Petasos Enhancement

--- src/main/java/net/fhirfactory/pegacorn/deployment/properties/CommunicateProperties.java
+++ src/main/java/net/fhirfactory/pegacorn/deployment/properties/CommunicateProperties.java
@@ -1,47 +1,25 @@
 /*
- * Copyright (c) 2020 MAHun
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-
 package net.fhirfactory.pegacorn.deployment.properties;
 
-import net.fhirfactory.pegacorn.common.model.FDN;
-import net.fhirfactory.pegacorn.common.model.RDN;
-
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
+
+/**
+ *
+ * @author Mark A. Hunter (ACT Health)
+ */
 
 @ApplicationScoped
 public class CommunicateProperties {
-    @Inject
-    SystemWideProperties siteCommonProperties;
+    
 
-    private static final String COMMUNICATE_SUBSYSTEM_DEPLOYMENT_NAME = "Pegacorn-Communicate(R1.0.0-SNAPSHOT)";
+    // Site Details
+    private static String ROOMSERVER_IDENTITY_PREFIX = "CHS";
 
-    public String getCommunicateSubsystemDeploymentName(){
-        return(COMMUNICATE_SUBSYSTEM_DEPLOYMENT_NAME);
-    }
-
-    public FDN getCommunicateSubsystemDeploymentInstanceID(){
-        FDN newFDN = siteCommonProperties.getSystemDeploymentInstanceID();
-        newFDN.appendRDN(new RDN(siteCommonProperties.getSubsystemQualifier(), getCommunicateSubsystemDeploymentName()));
-        return(newFDN);
+    public static String getRoomserverIdentityPrefix() {
+        return ROOMSERVER_IDENTITY_PREFIX;
     }
 }
